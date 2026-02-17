04ef3220e4290a20871e2a31ce4349428fcaf2cc
mascuud0715-arch <mascuud0715@gmail.com>
2026-02-17 20:51:17 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    await update.message.reply_text("Downloading video...")
+
+    file_name = "video.mp4"
+
+    ydl_opts = {
+        'outtmpl': file_name,
+        'format': 'best'
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        await update.message.reply_video(video=open(file_name, 'rb'))
+
+        os.remove(file_name)
+
+    except Exception as e:
+        await update.message.reply_text("Error downloading video")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, download_video))
+
+app.run_polling()
